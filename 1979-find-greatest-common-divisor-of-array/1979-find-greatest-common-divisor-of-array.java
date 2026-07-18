4d7e58b2dab68fb430e0b0f2fee222ea087695d9
swayamkr03 <swayamkumar312@gmail.com>
2026-07-19 04:05:37 +0530

    Time: 6 ms (18.8%), Space: 45.8 MB (7.89%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+
+        return gcd(nums[0],nums[n-1]);
+    }
+    public int gcd(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+
+}
